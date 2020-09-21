31d3b5ba4f00e81c3e0cea0003da722e3da3533e
Lucien George <luciengeorge95@gmail.com>
2020-09-21 21:33:45 +0100

    default url options and force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: "https://recipes.lewagon.com" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
