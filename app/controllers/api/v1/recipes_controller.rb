58d6df8a5ea2ec940b698e225c396fc5141df9aa
Lucien George <luciengeorge95@gmail.com>
2020-09-24 13:20:54 +0100

    search normally and with ajax

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipesController < Api::V1::BaseController
   def index
-    @pagy, @recipes = pagy(Recipe.all)
+    session[:query] = params[:query].present? ? params[:query] : nil
+    @pagy, @recipes = pagy(session[:query].present? ? Recipe.search(session[:query]) : Recipe.all, link_extra: "data-remote='true' data-action='click->recipes#renderRecipes' data-target='recipes.paginationLink'")
   end
 
   def show
--- app/javascript/controllers/recipes_controller.js
+++ app/javascript/controllers/recipes_controller.js
@@ -1,11 +1,28 @@
-import { Controller } from "stimulus"
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  renderRecipes() {
-    fetch(this.data.get('url'))
+  static targets = [ 'container', 'spinner', 'form', 'query', 'paginationLink' ];
+
+  renderRecipes(event) {
+    const paginationLink = this.paginationLinkTargets.find(link => link === event.currentTarget);
+    let page = 'page=1';
+    if (paginationLink) {
+      page = paginationLink.href.split('?')[1];
+    } else if (window.location.search.match(/page=\d+/)[0]) {
+      page = window.location.search.match(/page=\d+/)[0];
+    }
+    this.spinnerTarget.classList.remove('d-none');
+    this.containerTarget.innerHTML = '';
+    const url = this.hasQueryTarget && this.queryTarget.value ? `${this.data.get('url')}?query=${this.queryTarget.value}&${page}` : `${this.data.get('url')}?${page}`;
+    const redirectUrl = this.hasQueryTarget && this.queryTarget.value ? `${window.location.pathname}?query=${this.queryTarget.value}&${page}` : `${window.location.pathname}?${page}`;
+    fetch(url)
       .then(response => response.json())
       .then((data) => {
-        this.element.innerHTML = data.recipes;
+        this.spinnerTarget.classList.add('d-none');
+        this.containerTarget.innerHTML = data.recipes;
+        history.pushState({
+          id: 'recipe'
+        }, 'Recipes To Scrape', redirectUrl)
       });
   }
 
